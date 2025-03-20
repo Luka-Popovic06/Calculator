c07eb8fd0afffdaf36dc88603580a08a5bbce01b
Luka Popovic <lukapopovic0111@gmail.com>
2025-03-20 20:07:08 +0100

    New variables

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
-let amount = querySelector("#amount");
-let tipPercentage = querySelector("#percentage");
-let total = querySelector("#total");
+let amount = document.querySelector("#amount");
+let tipPercentage = document.querySelector("#percentage");
+let total = (document.querySelector("#total").value = 23);
+console.log(total);
