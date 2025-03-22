243dd473133b113c73d23aa087e223fe9cfdaa45
Luka Popovic <lukapopovic0111@gmail.com>
2025-03-22 16:56:45 +0100

    Multiplication

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let numberTwo = document.querySelector('#number-two');
 let total = document.querySelector('#total');
 let button = document.querySelector('#btn');
 let buttonTwo = document.querySelector('#btn-2');
+let buttonThree = document.querySelector('#btn-3');
+let buttonFour = document.querySelector('#btn-4');
 
 function calculateTotalOne() {
   const valueOfNumberOne = Number(numberOne.value);
@@ -11,7 +13,6 @@ function calculateTotalOne() {
   const calculator = valueOfNumberOne + valueOfNumberTwo;
   total.textContent = calculator;
 }
-
 button.addEventListener('click', calculateTotalOne);
 
 function calculateTotalTwo() {
@@ -21,3 +22,11 @@ function calculateTotalTwo() {
   total.textContent = calculatorTwo;
 }
 buttonTwo.addEventListener('click', calculateTotalTwo);
+
+function calculateTotalThree() {
+  const multiplicationOne = Number(numberOne.value);
+  const multiplicationTwo = Number(numberTwo.value);
+  const calculatorThree = multiplicationOne * multiplicationTwo;
+  total.textContent = calculatorThree;
+}
+buttonThree.addEventListener('click', calculateTotalThree);
