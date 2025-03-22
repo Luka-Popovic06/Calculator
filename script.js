47fa1e115e4e9519a1a8fd9a67634746fd9b0c5c
Luka Popovic <lukapopovic0111@gmail.com>
2025-03-22 15:59:31 +0100

    Makeing function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-"use strict";
-let amount = document.querySelector("#amount");
-let tipPercentage = document.querySelector("#percentage");
-let total = (document.querySelector("#total").value = 23);
-console.log(total);
+'use strict';
+let amount = document.querySelector('#amount');
+let tipPercentage = document.querySelector('#percentage');
+let total = document.querySelector('#total');
+let button = document.querySelector('#btn');
+
+function calculateTotal() {
+  const valueOfAmount = Number(amount.value);
+  const valueOfTipePercentage = Number(tipPercentage.value);
+  const calculator = valueOfAmount + tipPercentage;
+  total.textContent = calculator;
+}
